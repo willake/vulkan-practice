665040ce277fcc7bb57b927f45412223a87df830
Hui En <willakelin@gmail.com>
2023-07-11 17:12:58 +0800

    update: create vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,64 @@ private:
 
     void initVulkan()
     {
+        createVkInstance();
+    }
+
+    void createVkInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledLayerCount = 0;
+
+        // for MacOS
+        std::vector<const char *> requiredExtensions;
+
+        for (uint32_t i = 0; i < glfwExtensionCount; i++)
+        {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+        createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance))
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+        // checking for extension support
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions: \n";
+
+        for (const auto &extension : extensions)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
     }
 
     void mainLoop()
@@ -49,6 +107,7 @@ private:
     GLFWwindow *window;
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
+    VkInstance instance;
 };
 
 int main()
