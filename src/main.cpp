53ef76f8cfc6a20b6e2bd5c52d4c7b0c93d9832d
Hui En <willakelin@gmail.com>
2023-07-12 19:00:11 +0800

    update: queue families

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <map>
+#include <optional>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -91,6 +92,42 @@ private:
         glfwTerminate();
     }
 
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete() { return graphicsFamily.has_value(); }
+    };
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices;
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto &queueFamily : queueFamilies)
+        {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            {
+                indices.graphicsFamily = i;
+            }
+
+            if (indices.isComplete())
+            {
+                break;
+            }
+
+            i++;
+        }
+        // Logic to find graphics queue family indices to populate struct with
+        return indices;
+    }
+
     void pickPhysicalDevice()
     {
         uint32_t deviceCount = 0;
@@ -102,6 +139,7 @@ private:
         }
 
         std::vector<VkPhysicalDevice> devices(deviceCount);
+
         vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
         // Use an ordered map to automatically sort candidates by increasing score
@@ -114,11 +152,14 @@ private:
         }
 
         // Check if the best candidate is suitable at all
-        if (candidates.rbegin()->first > 0)
-        {
-            physicalDevice = candidates.rbegin()->second;
-        }
-        else
+        // if (candidates.rbegin()->first > 0)
+        // {
+        //     physicalDevice = candidates.rbegin()->second;
+        // }
+
+        physicalDevice = candidates.rbegin()->second;
+
+        if (physicalDevice == VK_NULL_HANDLE)
         {
             throw std::runtime_error("failed to find a suitable GPU!");
         }
@@ -136,6 +177,13 @@ private:
 
         // Discrete GPUs have a significant performance advantage
         if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+        {
+            score += 1000;
+        }
+
+        QueueFamilyIndices indices = findQueueFamilies(device);
+
+        if (indices.isComplete())
         {
             score += 1000;
         }
