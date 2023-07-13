4fb89d16ac6bad5a510a8c67c53c7fe9ed1a8823
Hui En <willakelin@gmail.com>
2023-07-13 10:16:04 +0800

    update: querying details of swap chain support

--- src/main.cpp
+++ src/main.cpp
@@ -109,6 +109,40 @@ private:
         }
     }
 
+    struct SwapChainSupportDetails
+    {
+        VkSurfaceCapabilitiesKHR capabilities;
+        std::vector<VkSurfaceFormatKHR> formats;
+        std::vector<VkPresentModeKHR> presentModes;
+    };
+
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
+    {
+        SwapChainSupportDetails details;
+
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+        uint32_t formatCount;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+
+        if (formatCount != 0)
+        {
+            details.formats.resize(formatCount);
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+        }
+
+        uint32_t presentModeCount;
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+
+        if (presentModeCount != 0)
+        {
+            details.presentModes.resize(presentModeCount);
+            vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+        }
+
+        return details;
+    }
+
     struct QueueFamilyIndices
     {
         std::optional<uint32_t> graphicsFamily;
@@ -117,7 +151,8 @@ private:
         bool isComplete() { return graphicsFamily.has_value() & presentFamily.has_value(); }
     };
 
-    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
+    QueueFamilyIndices
+    findQueueFamilies(VkPhysicalDevice device)
     {
         QueueFamilyIndices indices;
 
@@ -270,6 +305,13 @@ private:
         if (checkDeviceExtensionSupport(device))
         {
             score += 2000;
+
+            SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+            if (!swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty())
+            {
+                // swap chain support is adequate
+                score += 1000;
+            }
         }
 
         // Application can't function without geometry shaders
