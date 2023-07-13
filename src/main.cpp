4ce3b8e4bf78ddfdf2623f3813a759a1b32f8748
Hui En <willakelin@gmail.com>
2023-07-13 13:23:16 +0800

    update: fix file path

--- src/main.cpp
+++ src/main.cpp
@@ -117,8 +117,8 @@ private:
 
     void createGraphicsPipeline()
     {
-        auto vertShaderCode = readFile("shaders/vert.spv");
-        auto fragShaderCode = readFile("shaders/frag.spv");
+        auto vertShaderCode = readFile("../src/shaders/vert.spv");
+        auto fragShaderCode = readFile("../src/shaders/frag.spv");
 
         VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
