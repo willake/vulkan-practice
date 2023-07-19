490fb1da908b20bdca8cf11e25f3647c1c83baec
Hui En <willakelin@gmail.com>
2023-07-19 15:30:21 +0800

    update: try change colors

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ struct Vertex
 };
 
 const std::vector<Vertex> vertices = {
-    {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{0.0f, -0.5f}, {1.0f, 1.0f, 1.0f}},
     {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
     {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
 
